85b6a7e70043f5d41213c40ec33d6b5bce43f8e0
Becca <rkinnee91@hotmail.com>
2023-03-08 14:14:23 -0800

    build out carousel. ANNOYINGgit status!

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -11,15 +11,30 @@ function MainPage() {
     return (
         <section>
             <div className='mainPage'>
-                <h3 className='mainPage__subTitle'>Who We Are</h3>
-                <div className='mainPage__mainPara'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Suspendisse sem odio, malesuada eget lectus eu, interdum maximus risus. 
-                    Sed facilisis malesuada metus vel venenatis. 
-                    Aenean vel enim vel sapien cursus fermentum. 
-                    Nullam at metus eget elit commodo interdum.
+                    <h3 className='mainPage__subTitle'>Who We Are</h3>
+                <div className='mainPage__mainContainer'>
+                    <div className='mainPage__mainPara'>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Suspendisse sem odio, malesuada eget lectus eu, interdum maximus risus. 
+                        Sed facilisis malesuada metus vel venenatis. 
+                        Aenean vel enim vel sapien cursus fermentum. 
+                        Nullam at metus eget elit commodo interdum.
+                        <br/>
+                        <br/>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Suspendisse sem odio, malesuada eget lectus eu, interdum maximus risus. 
+                        Sed facilisis malesuada metus vel venenatis. 
+                        Aenean vel enim vel sapien cursus fermentum. 
+                        Nullam at metus eget elit commodo interdum.
+                        <br/>
+                        <br/>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Suspendisse sem odio, malesuada eget lectus eu, interdum maximus risus. 
+                        Sed facilisis malesuada metus vel venenatis. 
+
+                    </div>
+                    <img src={Img1} alt='' className='mainPage__mainImage'/>
                 </div>
-                <img src={Img1} alt='' className='mainPage__mainImage'/>
             </div>
             <TrendingContainer/>
         </section>
--- src/Components/MainPage/TrendingContainer/TrendingContainer.js
+++ src/Components/MainPage/TrendingContainer/TrendingContainer.js
@@ -1,32 +1,66 @@
 import './TrendingContainer.scss'
+import { useEffect, useState } from 'react';
 
 // Components
 import FashionData from './FashionData.json'
 
+const Card = (props) => {
+    return (
+        <li className="trending__card">
+            <p>{props.description}</p>
+            <img 
+                src={props.image}
+                alt={props.alt}
+                className="trending__trendImg"    
+            />
+        </li>
+    )
+}
+
 function Trending () {
+    const [moveClass, setMoveClass] = useState('');
+    const [carouselItems, setCarouselItems] = useState(FashionData);
+
+    useEffect(() => {
+        document.documentElement.style.setProperty('--num', carouselItems.length);
+    }, [carouselItems])
+    const handleAnimationEnd = () => {
+        if(moveClass === 'prev') {
+            shiftNext([...carouselItems])
+        } else if (moveClass === 'next') {
+            shiftPrev([...carouselItems])
+        }
+        setMoveClass('')
+    };
+    const shiftPrev = (copy) => {
+        let lastCard = copy.pop();
+        copy.splice(0, 0, lastCard);
+        setCarouselItems(copy)
+    }
+    const shiftNext = (copy) => {
+        let firstCard = copy.shift();
+        copy.splice(copy.length, 0, firstCard);
+        setCarouselItems(copy);
+    }
 
     return(
-        <div className="trending">
+        <div className="trending  module-wrapper">
             <h3 className="trending__subTitle">What's Trending</h3>
-            {
-                FashionData && FashionData.map((data, index) => {
-                    return(
-                        <div 
-                            className="trending__trendContainer" 
-                            key={data.id}
-                        >
-                            <div className="trending__description">
-                                {data.description}
-                            </div>
-                            <img 
-                                src={data.image}
-                                alt={data.alt}
-                                className="trending__trendImg"    
-                            />
-                        </div>
-                    )
-                })
-            }
+            <div className='trending__trendContain'>
+                <div className='trending__buttonContain'>
+                    <button onClick={() => setMoveClass('next')} className='trending__prev prev trending__btn'>
+                        Prev
+                    </button>
+                    <button onClick={() => setMoveClass('prev')} className='trending__next next trending__btn'>
+                        Next
+                    </button>
+                </div>
+                <ul onAnimationEnd={handleAnimationEnd} className={`${moveClass} trending__carousel`}>
+                    {carouselItems.map((i) => 
+                        <Card key={i.id} image={i.image} description={i.description}/>
+                    )}
+                </ul>
+            </div>
         </div>
     )
 }
